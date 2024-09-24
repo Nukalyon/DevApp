f0b80437033de740d5b190147d360c75ec59f32b
Nukalyon <56563361+Nukalyon@users.noreply.github.com>
2024-09-24 13:38:33 -0400

    Rendu Travail 7

--- GestionPersonne/src/main/java/com/example/gestionpersonne/Personne.java
+++ GestionPersonne/src/main/java/com/example/gestionpersonne/Personne.java
@@ -1,8 +1,23 @@
 package com.example.gestionpersonne;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Personne implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String nom;
+    private int age;
+    private String mail;
 
+    public Personne(String nom, int age, String mail) {
+        this.nom = nom;
+        this.age = age;
+        this.mail = mail;
+    }
 
+    @Override
+    public String toString() {
+        return "Nom: " + this.nom + ", Age: " + this.age + ", Mail: " + this.mail;
+    }
 }
--- GestionPersonne/src/main/java/com/example/gestionpersonne/PersonneController.java
+++ GestionPersonne/src/main/java/com/example/gestionpersonne/PersonneController.java
@@ -4,7 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.*;
+
 public class PersonneController {
+    //private final String PATH = "GestionPersonne/src/";
+    private final String NOM_FICHIER_TEXTE = "personneTexte.txt";
+    private final String NOM_FICHIER_BINAIRE = "personneBinary.bin";
+    private final String NOM_FICHIER_SERIALISE = "personneSerialise.ser";
+    @FXML
+    TextField txtFieldLog;
     @FXML
     TextField txtFieldMail;
     @FXML
@@ -12,13 +20,77 @@ public class PersonneController {
     @FXML
     TextField txtFieldName;
 
-    public void serialize(ActionEvent actionEvent) {
+    public void serialize(ActionEvent ignoredActionEvent) {
+        try{
+            File f = new File(NOM_FICHIER_SERIALISE);
+
+            if(!isFileExisting(f)){
+                //on le créée
+                f.createNewFile();
+            }
+            //Ici on sait que le fichier est présent
+            Personne p = new Personne(txtFieldName.getText(), Integer.parseInt(txtFieldAge.getText()), txtFieldMail.getText());
+            FileOutputStream fos = new FileOutputStream(NOM_FICHIER_SERIALISE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(p);
+            oos.close();
+            fos.close();
+            txtFieldLog.setText("Objet sérialisé");
+        }
+        catch (Exception exc){
+            txtFieldLog.setText("Exception: " + exc.getMessage());
+        }
     }
 
-    public void registerBin(ActionEvent actionEvent) {
+    public void registerBin(ActionEvent ignoredActionEvent) {
+        try{
+            File f = new File(NOM_FICHIER_BINAIRE);
+            if(isFileExisting(f)){
+                //on le créée
+                f.createNewFile();
+            }
+            //Ici on sait que le fichier est présent
+            Personne p = new Personne(txtFieldName.getText(), Integer.parseInt(txtFieldAge.getText()), txtFieldMail.getText());
+            FileOutputStream fos = new FileOutputStream(NOM_FICHIER_BINAIRE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(p);
+            oos.close();
+            fos.close();
+            txtFieldLog.setText("Objet binarisé");
+        }
+        catch (Exception exc){
+            txtFieldLog.setText("Exception: " + exc.getMessage());
+        }
     }
 
-    public void registerTxt(ActionEvent actionEvent) {
+    public void registerTxt(ActionEvent ignoredActionEvent) {
+        try{
+            File f = new File(NOM_FICHIER_TEXTE);
 
+            if(!isFileExisting(f)){
+                //on le créée
+                f.createNewFile();
+            }
+            //Ici on sait que le fichier est présent
+            Personne p = new Personne(txtFieldName.getText(), Integer.parseInt(txtFieldAge.getText()), txtFieldMail.getText());
+            FileWriter fw = new FileWriter(NOM_FICHIER_TEXTE);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(p.toString());
+            bw.close();
+            fw.close();
+            txtFieldLog.setText("Objet textualisé");
+        }
+        catch (Exception exc){
+            txtFieldLog.setText("Exception: " + exc.getMessage());
+        }
+    }
+
+    private boolean isFileExisting(File f) {
+        boolean res = true;
+        if(!f.exists() || !f.isFile()){
+            //N'existe pas ou n'est pas un fichier
+            res = false;
+        }
+        return res;
     }
 }
