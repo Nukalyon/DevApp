f775294c989721e7f4e51571612d19c3c7cf4199
Nukalyon <56563361+Nukalyon@users.noreply.github.com>
2024-11-12 19:46:19 -0500

    MainClass
    Cleanup

--- ZombieShooter/core/src/main/java/educ/lasalle/ZombieShooter.java
+++ ZombieShooter/core/src/main/java/educ/lasalle/ZombieShooter.java
@@ -1,35 +1,14 @@
 package educ.lasalle;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import educ.lasalle.controller.BulletController;
-import educ.lasalle.controller.PlayerController;
-import educ.lasalle.controller.ZombieController;
-import educ.lasalle.manager.AssetManager;
-import educ.lasalle.manager.CooldownManager;
-import educ.lasalle.manager.ScoreManager;
-import educ.lasalle.model.Bullet;
-import educ.lasalle.model.Player;
-import educ.lasalle.model.Zombie;
 import educ.lasalle.ui.GameScreen;
-
-import java.util.ArrayList;
 
 /**
  * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
  */
 public class ZombieShooter extends Game {
+    // Used by all screens
     public SpriteBatch batch;
 
     @Override
@@ -43,16 +22,4 @@ public class ZombieShooter extends Game {
     {
         super.render();
     }
-
-    @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-    }
-
-
 }
